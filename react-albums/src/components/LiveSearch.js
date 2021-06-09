ed8e4aa78b79f602fd990081ad17d0196540f30d
tandica <williams.tandica@gmail.com>
2021-06-09 13:48:08 -0400

    used axios and useEffect to display albums by search

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,5 +1,5 @@
-import React, { Fragment, useState } from "react";
-
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
 
@@ -7,13 +7,22 @@ export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
 
+  useEffect(() => {
+    const testURL =
+    `https://itunes.apple.com/search?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`;
+    axios.get(testURL).then((response) => {
+      console.log(response);
+      setResults([...response.data.results])
+    });
+  }, [term]);
+
   return (
     <Fragment>
       <header className="logo">
         <img src="images/brand.png" alt="Brand" />
       </header>
       <main>
-        <SearchBar onSearch={term => setTerm(term)} />
+        <SearchBar onSearch={(term) => setTerm(term)} />
         <Results results={results} />
       </main>
     </Fragment>
